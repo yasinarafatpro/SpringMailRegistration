ba9daa511d58e452c8d80d29ded29d12fb55b64e
Yasin Arafat <yasinarafat1194@gmail.com>
2021-06-09 14:11:11 +0600

    service and security

---
+++ src/main/java/com/example/demo/controller/RegistrationService.java
@@ -0,0 +1,8 @@
+package com.example.demo.controller;
+
+
+public class RegistrationService {
+    public String register(RegistrationRequest request){
+        return "works";
+    }
+}
---
+++ src/main/java/com/example/demo/security/config/WebSecurityConfig.java
@@ -0,0 +1,4 @@
+package com.example.demo.security.config;
+
+public class WebSecurityConfig {
+}
